6509d1c91ab4af0edb2a0489a8b3d4a3987397c7
Prathmesh Sadake <prathmeshsadake@gmail.com>
2020-11-29 17:03:48 +0530

    Done with basic working prototype for about section

---
+++ src/components/About/About.component.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+
+import SocialIcons from '../SocialIcons/SocialIcons.component';
+
+import './about.styles.scss';
+
+export default class About extends Component {
+    render() {
+        return (
+           <div className="section-about">
+                <Container>
+                <Row>
+                    <Col sm={12} md={8} lg={6}>
+                        <h2 className="heading-about">
+                            about me
+                        </h2>
+                        <p className="copy-text">
+                         Hi I'm Prathmesh, a Frontend Web Developer & Computer Science Engineer
+                         who loves building webpages and Software products. I’d love to combine my
+                         passion for learning and developing with my software development skills
+                         to continue building personalized products for people
+                        </p>
+                        <SocialIcons />
+                    </Col>
+                    <Col sm={12} md={8} lg={6}>
+                        <div className="section-about-img">
+                            <img src={process.env.PUBLIC_URL + '/assets/images/about-section.jpeg'} alt="demo" width="85%"/>
+                        </div>
+                    </Col>
+                </Row>
+                </Container>
+           </div>
+        )
+    }
+}
