a497dbe5ade17af0f939d347feaf065b93f12284
Prathmesh Sadake <prathmeshsadake@gmail.com>
2020-11-29 21:09:24 +0530

    Completed about section

--- src/components/About/About.component.jsx
+++ src/components/About/About.component.jsx
@@ -8,24 +8,24 @@ import './about.styles.scss';
 export default class About extends Component {
     render() {
         return (
-           <div className="section-about">
+           <div className="section-about" id="about">
                 <Container>
                 <Row>
-                    <Col sm={12} md={8} lg={6}>
+                    <Col sm={12} md={12} lg={6}>
                         <h2 className="heading-about">
-                            about me
+                            about
                         </h2>
                         <p className="copy-text">
                          Hi I'm Prathmesh, a Frontend Web Developer & Computer Science Engineer
                          who loves building webpages and Software products. I’d love to combine my
                          passion for learning and developing with my software development skills
-                         to continue building personalized products for people
+                         to continue building personalized products for people.
                         </p>
-                        <SocialIcons />
+                        <SocialIcons/>
                     </Col>
-                    <Col sm={12} md={8} lg={6}>
+                    <Col sm={12} md={12} lg={6}>
                         <div className="section-about-img">
-                            <img src={process.env.PUBLIC_URL + '/assets/images/about-section.jpeg'} alt="demo" width="85%"/>
+                            <img src={process.env.PUBLIC_URL + '/assets/images/about-section.jpeg'} alt="demo"/>
                         </div>
                     </Col>
                 </Row>
