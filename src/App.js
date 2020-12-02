1f70b330937f5a4371065506f8f64200d0e330be
Prathmesh Sadake <prathmeshsadake@gmail.com>
2020-12-02 23:21:07 +0530

    imported components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import Header from './components/Header/Header.component';
+import About from './components/About/About.component';
+import Resume from './components/Resume/Resume.component';
+import Footer from './components/Footer/Footer.component';
 import './App.scss';
 
 class App extends Component  {
@@ -7,6 +10,9 @@ class App extends Component  {
   return (
     <div className="App">
       <Header />
+      <About />
+      <Resume />
+      <Footer />
     </div>
   );
 }
