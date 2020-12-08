6290b31ca3fd390e07b15051fa36bb17bdc90f1c
Prathmesh Sadake <prathmeshsadake@gmail.com>
2020-12-08 16:55:41 +0530

    just gien height and bg color

---
+++ src/components/Footer/Footer.component.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './footer.styles.scss';
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className="section-footer">
+                
+            </div>
+        )
+    }
+}
