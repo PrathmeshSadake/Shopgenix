18e0b90e780de21d0726ee670854444025f56886
Prathmesh Sadake <prathmeshsadake@gmail.com>
2020-12-30 14:18:08 +0530

    completed nav bar with react-router

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,6 +3,10 @@ import './nav-styles.css';
 
 import { Link } from 'react-router-dom'
 
+import menuIcon from '../img/menu.svg';
+import logo from '../img/logo.svg'
+import styled from 'styled-components';
+
 const Nav = () => {
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
@@ -11,21 +15,42 @@ function openNav() {
     document.getElementById("myNav").style.width = "0%";
   }
     return(
-        <div>
+        <StyledNav>
+          <div>
+            <img src={logo} width="45px" alt="navbar-logo"/>
+            <StyledNameLogo> Prathmesh.</StyledNameLogo>
+          </div>
             <div id="myNav" className="overlay">
 <Link className="closebtn" onClick={closeNav}>&times;</Link>
 <div className="overlay-content">
-  <Link to="/">About</Link>
-  <Link to="/">Services</Link>
-  <Link to="/">Clients</Link>
-  <Link to="/">Contact</Link>
+  <Link to="/" onClick={closeNav}>About</Link>
+  <Link to="/" onClick={closeNav}>Services</Link>
+  <Link to="/" onClick={closeNav}>Clients</Link>
+  <Link to="/contact" onClick={closeNav}>Contact</Link>
 </div>
 
 </div>
 
-<Link  href="#f" onClick={openNav}>&times;</Link>
-        </div>
+<Link  href="#f" onClick={openNav}>
+  <img src={menuIcon} alt="menu icon" height="40"/>
+</Link>
+        </StyledNav>
     );
 }
+
+
+const StyledNav = styled.nav`
+  display:flex;
+  justify-content: space-between;
+  padding: 1rem 8rem 0.2rem 8rem;
+  align-items:center;
+  color:#fff;
+  background-color: #1f2833; 
+`;
+
+const StyledNameLogo = styled.span`
+  margin-top : -26px;
+  font-size: 35px;
+`;
 
 export default Nav;
