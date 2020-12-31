353aef3374125440d019ddece2f2bef9ebb27f04
Prathmesh Sadake <prathmeshsadake@gmail.com>
2020-12-31 12:56:41 +0530

    Added contact section wave

--- src/components/Wave.js
+++ src/components/Wave.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import {motion} from 'framer-motion';
 
-const Wave = () => {
+export function Wave(){
     return(
         <StyledWaveSvg viewBox="0 0 1440 363" fill="none" xmlns="http://www.w3.org/2000/svg">
         <motion.path initial={{pathLength:0, pathOffset:1}} animate={{pathLength:1, pathOffset:0}} transition={{duration:1}} d="M1440 27.4774C1352.73 19.8184 1122.41 49.0556 899.331 227.276C620.48 450.052 354.282 355.647 170.328 185.318C23.165 49.0556 -4.21721 8.32998 0.487081 5" stroke="#66fcf1" strokeOpacity="0.5" strokeWidth="5"/>
@@ -16,4 +16,14 @@ left:0;
 z-index: -10;
 `;
 
-export default Wave;
+export function Wave2(){
+    return(
+        <StyledWaveSvg2 viewBox="0 0 1440 363" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <motion.path initial={{pathLength:0, pathOffset:1}} animate={{pathLength:1, pathOffset:0}} transition={{duration:1}} d="M1440 27.4774C1352.73 19.8184 1122.41 49.0556 899.331 227.276C620.48 450.052 354.282 355.647 170.328 185.318C23.165 49.0556 -4.21721 8.32998 0.487081 5" stroke="#ffc6c1" strokeOpacity="0.5" strokeWidth="5"/>
+        </StyledWaveSvg2>
+    );
+}
+
+const StyledWaveSvg2 = styled(StyledWaveSvg)`
+z-index: 100;
+`;
