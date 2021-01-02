6d9a92211e128e38d562d5410007ede2529c7a03
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-01-02 11:40:25 +0530

    Added links to social icons

--- src/components/SocialComponent.js
+++ src/components/SocialComponent.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import {Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { AiFillInstagram, AiFillTwitterCircle, AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import { SiGmail } from "react-icons/si";
 
-export default function SocialComponent({icon}) {
+export default function SocialComponent() {
 
     return (
         <StyledSocialDiv>
             <StyledSocialSpan>
-            <Link className="social-icon" ><AiFillLinkedin size='40'/></Link>
-            <Link className="social-icon" ><SiGmail size="40"/></Link>
-            <Link className="social-icon" ><AiFillGithub size="40"/></Link>
-            <Link className="social-icon" ><AiFillInstagram size="40"/></Link>
-            <Link className="social-icon" ><AiFillTwitterCircle size="40"/></Link>
+            <a href="https://www.linkedin.com/in/prathmeshsadake" target="_blank" rel="noreferrer noopener" className="social-icon" ><AiFillLinkedin size='40'/></a>
+            <a href="mailto:prathmeshsadake@gmail.com" target="_blank" rel="noreferrer noopener" className="social-icon" ><SiGmail size="40"/></a>
+            <a href="https://www.github.com/prathmeshsadake" target="_blank" rel="noreferrer noopener" className="social-icon" ><AiFillGithub size="40"/></a>
+            <a href="https://www.instagram.com/prxthmesh" target="_blank" rel="noreferrer noopener" className="social-icon" ><AiFillInstagram size="40"/></a>
+            <a href="https://www.twitter.com/prathmeshsadake" target="_blank" rel="noreferrer noopener" className="social-icon" ><AiFillTwitterCircle size="40"/></a>
         </StyledSocialSpan>
         </StyledSocialDiv>
     );
@@ -31,9 +30,6 @@ margin: 2rem 0;
 
 
 const StyledSocialSpan = styled.div`
-    /* a{
-        text-decoration: none;
-    } */
     z-index: 5;
     .social-icon{
         margin: 0 1rem;
