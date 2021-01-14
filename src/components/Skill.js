bd29b9f02a0bf7be341765a0fc53ee0c84c44f4c
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-01-15 01:33:50 +0530

    created skill component

---
+++ src/components/Skill.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components';
+
+export default function Skill({skill}) {
+    return (
+        <StyledSkill>
+            {skill}      
+        </StyledSkill>
+    );
+}
+
+const StyledSkill = styled.div`
+    padding: 5px 10px;
+    display: inline;
+    color: white;
+    color: #000;
+    margin: 20px 20px 0 0;
+    border: 1px solid #a0a0a0;
+    box-shadow: 2px 2px #a0a0a0;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+`;
