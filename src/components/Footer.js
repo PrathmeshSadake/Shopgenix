6815a6f8b6ed5a4fb2406ffc58952b2b6b3cbf01
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-01-15 01:35:19 +0530

    Footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -40,6 +40,17 @@ const StyledFooterNav = styled.div`
             &::last-child{
                 margin-right: 0rem;
             }
+            a{
+                text-decoration: none;
+                font-size: 1.25rem;
+                letter-spacing: 1px;
+                font-weight: 500;
+                transition: all 1.5s ease-in;
+                &:hover{
+                    color: #4A65F2;
+                    border-bottom: 1.5px solid #5750EB;
+                }
+            }
         }
         
     }
