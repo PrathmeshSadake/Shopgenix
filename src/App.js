b7ecbe712e5a6b228b447b02bd855d9e762d2645
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-01-14 20:25:40 +0530

    Added BrowserRoute

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './app.css';
 import HomePage from './pages/Home';
+import { Switch, Route } from 'react-router-dom';
+
+import ContactPage from './pages/ContactPage';
+import ProjectsPage from './pages/Projects';
+import Footer from './components/Footer';
 
 function App(){
     return(
         <div>
+            <Switch>
+                <Route exact path='/'>
             <HomePage/>
+                </Route>
+                <Route path='/projects'>
+            <ProjectsPage/>
+                </Route>
+                <Route path='/contact'>
+            <ContactPage/>
+                </Route>
+            </Switch>
+        <Footer/>
         </div>
     );
 }
