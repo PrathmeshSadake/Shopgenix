b607f5fcb28d09f06176ac9670c980d64464a10c
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-01-15 01:34:57 +0530

    Seperated nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import logo from '../img/logo.svg';
+
+export default function Nav() {
+    return (
+        <StyledNav>
+                <Link to="/"><h1><img src={logo} alt='logo' height="25px"/> Prathmesh Sadake</h1></Link>
+                <ul>
+                    <li><Link to="/projects">Projects</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                </ul>
+            </StyledNav>
+    );
+}
+
+
+const StyledNav = styled.nav`
+    height:10vh;
+    display:flex;
+    margin: auto ;
+    justify-content: space-between;
+    align-items: center;
+    padding:0.25rem 0rem;
+    a{
+        text-decoration: none;
+    }
+    ul{
+        list-style: none;
+        display: flex;
+        li{
+            padding-left: 10rem;
+            a{
+                text-decoration: none;
+                font-size: 1.25rem;
+                letter-spacing: 1px;
+                font-weight: 500;
+                transition: all 1.5s ease-in;
+                &:hover{
+                    color: #4A65F2;
+                    border-bottom: 1.5px solid #5750EB;
+                }
+            }
+        }
+    }
+`;
