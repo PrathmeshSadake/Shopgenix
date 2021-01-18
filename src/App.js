3ee2e2b0f04be8ec7836845fe86e5fa608002459
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-01-18 20:57:30 +0530

    Added react-spinner

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './app.css';
+import { css } from "@emotion/core";
 import HomePage from './pages/Home';
 import { Switch, Route } from 'react-router-dom';
-
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 import ContactPage from './pages/ContactPage';
 import ProjectsPage from './pages/Projects';
 import Footer from './components/Footer';
 
+import styled from 'styled-components';
+
+
+const override = css`
+    display: block;
+`;
+
 function App(){
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        setTimeout(() => { setLoading(false) }, 4000);
+    });
+
     return(
+        <>{
+        loading ? 
+        <StyledLoaderDiv>
+        <ClimbingBoxLoader  color="#4A65F2" size="50" css={override} loading={loading}/> 
+        </StyledLoaderDiv>:
         <div>
             <Switch>
                 <Route exact path='/'>
@@ -23,7 +42,17 @@ function App(){
             </Switch>
         <Footer/>
         </div>
+        }
+        </>
     );
 }
+
+const StyledLoaderDiv = styled.div`
+    width: 100vw;
+    height:100vh;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+`;
 
 export default App;
